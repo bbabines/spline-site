459cb45405030bb0324d53d1f81ddbdd26939268
unknown <bbabines@gmail.com>
2023-03-15 12:02:39 -0400

    First commit.

---
+++ sections/index.js
@@ -0,0 +1,7 @@
+import Hero from "./Hero";
+import About from "./About";
+import Explore from "./Explore";
+import GetStarted from "./GetStarted";
+import Feedback from "./Feedback";
+
+export { Hero, About, Explore, GetStarted, WhatsNew, Feedback };
